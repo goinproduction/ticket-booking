36236c521bd4f7228d5aa6d93f817a3b297339ef
viincyy <viincyy126@gmail.com>
2021-10-11 00:18:43 +0700

    Fix bug play button carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,55 +4,76 @@ import slide2 from '../assets/img/slide03.png';
 import slide3 from '../assets/img/slide04.png';
 import slide4 from '../assets/img/slide06.png';
 import slide5 from '../assets/img/slide07.png';
+import play from '../assets/img/play-video.png';
 
 const Carousel = () => {
     return (
         <section className='carousel'>
             <div className='carousel__slider'>
-                <img src={slide1} className='carousel__slider-image' />
+                <img
+                    src={slide1}
+                    className='carousel__slider-image'
+                    alt='slide'
+                />
                 <button className='carousel__trailer'>
                     <img
-                        src='./assets/img/play-video.png'
+                        src={play}
                         alt='play'
                         className='carousel__trailer-play'
                     />
                 </button>
             </div>
             <div className='carousel__slider'>
-                <img src={slide2} className='carousel__slider-image' />
+                <img
+                    src={slide2}
+                    className='carousel__slider-image'
+                    alt='slide'
+                />
                 <button className='carousel__trailer'>
                     <img
-                        src='./assets/img/play-video.png'
+                        src={play}
                         alt='play'
                         className='carousel__trailer-play'
                     />
                 </button>
             </div>
             <div className='carousel__slider'>
-                <img src={slide3} className='carousel__slider-image' />
+                <img
+                    src={slide3}
+                    className='carousel__slider-image'
+                    alt='slide'
+                />
                 <button className='carousel__trailer'>
                     <img
-                        src='./assets/img/play-video.png'
+                        src={play}
                         alt='play'
                         className='carousel__trailer-play'
                     />
                 </button>
             </div>
             <div className='carousel__slider'>
-                <img src={slide4} className='carousel__slider-image' />
+                <img
+                    src={slide4}
+                    className='carousel__slider-image'
+                    alt='slide'
+                />
                 <button className='carousel__trailer'>
                     <img
-                        src='./assets/img/play-video.png'
+                        src={play}
                         alt='play'
                         className='carousel__trailer-play'
                     />
                 </button>
             </div>
             <div className='carousel__slider'>
-                <img src={slide5} className='carousel__slider-image' />
+                <img
+                    src={slide5}
+                    className='carousel__slider-image'
+                    alt='slide'
+                />
                 <button className='carousel__trailer'>
                     <img
-                        src='./assets/img/play-video.png'
+                        src={play}
                         alt='play'
                         className='carousel__trailer-play'
                     />
